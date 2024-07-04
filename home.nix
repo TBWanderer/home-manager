0d5a9428d33227fe3259c106cf5ead29c251f2f1
TBWanderer <evgenypodyryaka@yandex.ru>
2024-07-04 17:03:35 +0300

    Fixed xdg

--- home.nix
+++ home.nix
@@ -6,6 +6,9 @@
   home.username = "x";
   home.homeDirectory = "/home/x";
 
+  xdg.userDirs = {
+    enable = true;
+  };
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
